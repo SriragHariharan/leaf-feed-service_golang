4470beab6ee4e20138733549da65eb4b03993001
Srirag Hariharan <sriraghariharan108@gmail.com>
2026-05-13 15:09:54 +0530

    Added AuthorId to be populated in the feed.

--- internal/models/feed.go
+++ internal/models/feed.go
@@ -3,16 +3,17 @@ package models
 import "time"
 
 type Feed struct {
-	FeedID    string    `json:"feed_id" gorm:"primaryKey"`
-	UserID    string    `json:"user_id" gorm:"not null"`
-	PostID    string    `json:"post_id" gorm:"not null"`
-	IsLiked   bool      `json:"is_liked" gorm:"not null"`
-	IsCommented bool      `json:"is_commented" gorm:"not null"`
-	CreatedAt time.Time `json:"created_at" gorm:"not null"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"not null"`
+    FeedID      string    `json:"feed_id" gorm:"primaryKey"`
+    UserID      string    `json:"user_id" gorm:"not null"`
+    AuthorID    string    `json:"author_id" gorm:"not null"`
+    PostID      string    `json:"post_id" gorm:"not null"`
+    IsLiked     bool      `json:"is_liked" gorm:"not null"`
+    IsCommented bool      `json:"is_commented" gorm:"not null"`
+    CreatedAt   time.Time `json:"created_at" gorm:"not null"`
+    UpdatedAt   time.Time `json:"updated_at" gorm:"not null"`
 
-	User *User `gorm:"foreignKey:UserID"`
-	Post *Post `gorm:"foreignKey:PostID"`
+    Author *User `gorm:"foreignKey:UserID"`
+    Post   *Post `gorm:"foreignKey:PostID"`
 }
 
 func (Feed) TableName() string { return "feed_feeds" }
