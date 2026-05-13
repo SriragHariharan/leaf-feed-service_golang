09245b50f4c8a58268d224f0a991d5b08e35e09e
Srirag Hariharan <sriraghariharan108@gmail.com>
2026-05-13 10:14:48 +0530

    Created basic api endpoint.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	fmt.Println("Hello, Welcome to the Feed Service!")
+
+	//gorilla mux router
+	router := mux.NewRouter()
+	router.HandleFunc("/", homeHandler).Methods("GET")
+	log.Fatal(http.ListenAndServe(":4040", router))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Hello, Welcome to the Feed Service!"})
 }
